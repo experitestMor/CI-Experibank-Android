eb4abbfc7efcafb119172c242d47d7b3e9583358
mor.hazan <mor.hazan@experitest.com>
2018-07-24 11:33:05 +0300

    asf

--- scripts/upload-app.sh
+++ scripts/upload-app.sh
@@ -1,2 +1,1 @@
-APP_PATH=file=@$PWD/app/build/outputs/apk/app-debug.apk
-curl -k -X POST https://cloud.seetest.io/api/v1/applications/new -H "Authorization: Bearer $SEETEST_IO_ACCESS_KEY" -H "Cache-Control: no-cache" -H "content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" -F "$APP_PATH"
+curl -v https://sales.experitest.com/api/v1/applications/new?project=Default -H "Authorization: Bearer $accessKey" -F "@$PWD/app/build/outputs/apk/app-debug.apk"
